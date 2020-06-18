6ba89df39defa8ad88ac2eaff3702ce390df2535
LuisSic <lantoni24@hotmail.com>
2020-06-18 04:34:45 -0600

    React

---
+++ holamundo/src/components/HolaMundo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const HolaMundo = () => {
+  const Hello = 'Hola Mundo!';
+  const isTrue = false;
+  return (
+    <div className="HolaMundo">
+      <h1>{Hello}</h1>
+      <h2>Curso Esencial de React</h2>
+      <img src="https://arepa.s3.amazonaws.com/react.png" alt="react" />
+      {isTrue ? <h4>Esto es verdadero</h4> : <h5>Es falso</h5>}
+      {isTrue && <h4>Soy verdadero</h4>}
+    </div>
+  );
+};
+
+export default HolaMundo;
